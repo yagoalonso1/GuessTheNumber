700b2a942b5e3cd2ecc7e6d65196cdc705c4144f
Yago Alonso <yagoalonso3105@gmail.com>
2025-05-05 17:58:39 +0200

    fix(model): simplificación de los modelos que recibe la API

--- lib/model/character.dart
+++ lib/model/character.dart
@@ -1,68 +1,41 @@
 class Character {
-  final String id;
   final String name;
-  final List<String> alternateNames;
   final String species;
-  final String gender;
   final String house;
+  final String gender;
   final String dateOfBirth;
-  final int? yearOfBirth;
   final bool wizard;
   final String ancestry;
-  final String eyeColour;
-  final String hairColour;
-  final Wand wand;
-  final String patronus;
   final bool hogwartsStudent;
   final bool hogwartsStaff;
-  final String actor;
-  final List<String> alternateActors;
   final bool alive;
   final String image;
 
   Character({
-    required this.id,
     required this.name,
-    required this.alternateNames,
     required this.species,
-    required this.gender,
     required this.house,
+    required this.gender,
     required this.dateOfBirth,
-    this.yearOfBirth,
     required this.wizard,
     required this.ancestry,
-    required this.eyeColour,
-    required this.hairColour,
-    required this.wand,
-    required this.patronus,
     required this.hogwartsStudent,
     required this.hogwartsStaff,
-    required this.actor,
-    required this.alternateActors,
     required this.alive,
     required this.image,
   });
 
   factory Character.fromJson(Map<String, dynamic> json) {
     return Character(
-      id: json['id'] ?? '',
       name: json['name'] ?? '',
-      alternateNames: List<String>.from(json['alternate_names'] ?? []),
       species: json['species'] ?? '',
-      gender: json['gender'] ?? '',
       house: json['house'] ?? '',
+      gender: json['gender'] ?? '',
       dateOfBirth: json['dateOfBirth'] ?? '',
-      yearOfBirth: json['yearOfBirth'],
       wizard: json['wizard'] ?? false,
       ancestry: json['ancestry'] ?? '',
-      eyeColour: json['eyeColour'] ?? '',
-      hairColour: json['hairColour'] ?? '',
-      wand: Wand.fromJson(json['wand'] ?? {}),
-      patronus: json['patronus'] ?? '',
       hogwartsStudent: json['hogwartsStudent'] ?? false,
       hogwartsStaff: json['hogwartsStaff'] ?? false,
-      actor: json['actor'] ?? '',
-      alternateActors: List<String>.from(json['alternate_actors'] ?? []),
       alive: json['alive'] ?? true,
       image: json['image'] ?? '',
     );
@@ -70,54 +43,17 @@ class Character {
 
   Map<String, dynamic> toJson() {
     return {
-      'id': id,
       'name': name,
-      'alternate_names': alternateNames,
       'species': species,
-      'gender': gender,
       'house': house,
+      'gender': gender,
       'dateOfBirth': dateOfBirth,
-      'yearOfBirth': yearOfBirth,
       'wizard': wizard,
       'ancestry': ancestry,
-      'eyeColour': eyeColour,
-      'hairColour': hairColour,
-      'wand': wand.toJson(),
-      'patronus': patronus,
       'hogwartsStudent': hogwartsStudent,
       'hogwartsStaff': hogwartsStaff,
-      'actor': actor,
-      'alternate_actors': alternateActors,
       'alive': alive,
       'image': image,
-    };
-  }
-}
-
-class Wand {
-  final String wood;
-  final String core;
-  final double? length;
-
-  Wand({
-    required this.wood,
-    required this.core,
-    this.length,
-  });
-
-  factory Wand.fromJson(Map<String, dynamic> json) {
-    return Wand(
-      wood: json['wood'] ?? '',
-      core: json['core'] ?? '',
-      length: json['length'] != null ? double.tryParse(json['length'].toString()) : null,
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'wood': wood,
-      'core': core,
-      'length': length,
     };
   }
 } 
